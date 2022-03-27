4648fbc2490eaab5b4f4034d6871a3a85ee552f5
Tugrul Okumus <cto1997@gmail.com>
2022-03-27 16:57:45 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace static_sinif_ve_uyeler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Çalışan Sayısı: {0}", Calisan.CalisanSayisi);
+
+            Calisan calisan = new Calisan("Ayşe", "Yılmaz", "İK");
+            Console.WriteLine("Çalışan Sayısı: {0}", Calisan.CalisanSayisi);
+
+            Calisan calisan1 = new Calisan("Egemen", "Öztürk", "Algo Tech");
+            Calisan calisan2 = new Calisan("Deniz", "Burnu", "Management");
+            Console.WriteLine("Çalışan Sayısı: {0}", Calisan.CalisanSayisi);
+        }
+    }
+
+    class Calisan
+    {
+        private static int calisanSayisi;
+
+        public static int CalisanSayisi { get => calisanSayisi; }
+
+        private string Isim;
+        private string Soyisim;
+        private string Departman;
+
+        static Calisan()
+        {
+            calisanSayisi = 0;
+        }
+        public Calisan(string isim, string soyisim, string departman)
+        {
+            this.Isim = isim;
+            this.Soyisim = soyisim;
+            this.Departman = departman;
+            calisanSayisi++;
+        }
+    }
+}
